58875fa65ead7075eda7fbba993f1348f716e680
zhaoxinhao <zhaoxinhao2012@126.com>
2022-04-20 09:33:47 +0800

    add new experiment

--- experiments/0_20220418_dataprocess/sample.sh
+++
@@ -1,3 +0,0 @@
-python $CPM_BASE_PATH/experiments/0_20220418_dataprocess/code/sample.py \
-	--data-dir $CPM_DATA_PATH \
-	--file-name dev.jsonl.dedup
--- experiments/0_dataprocess/pre-tokenize.sh
+++ experiments/0_dataprocess/pre-tokenize.sh
@@ -1,4 +1,6 @@
-python $CPM_BASE_PATH/experiments/0_20220418_dataprocess/code/pre-tokenize.py \
+BASE_PATH=$(cd $(dirname "${BASH_SOURCE[0]}") >/dev/null && pwd)
+
+python $BASE_PATH/code/pre-tokenize.py \
 	--process-num 16 \
 	--data-dir $CPM_DATA_PATH \
 	--dataset LCSTS \
@@ -7,7 +9,7 @@ python $CPM_BASE_PATH/experiments/0_20220418_dataprocess/code/pre-tokenize.py \
 	--model-config cpm1-small \
 	--output-dir $CPM_TRAIN_DATA_PATH
 
-python $CPM_BASE_PATH/experiments/0_20220418_dataprocess/code/pre-tokenize.py \
+python $BASE_PATH/code/pre-tokenize.py \
 	--process-num 16 \
 	--data-dir $CPM_DATA_PATH \
 	--dataset LCSTS \
@@ -16,7 +18,7 @@ python $CPM_BASE_PATH/experiments/0_20220418_dataprocess/code/pre-tokenize.py \
 	--model-config cpm1-small \
 	--output-dir $CPM_TRAIN_DATA_PATH
 
-python $CPM_BASE_PATH/experiments/0_20220418_dataprocess/code/pre-tokenize.py \
+python $BASE_PATH/code/pre-tokenize.py \
 	--process-num 16 \
 	--data-dir $CPM_DATA_PATH \
 	--dataset LCSTS \
