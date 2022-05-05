dd1542cf0395508d2e6e739742458f804110c584
zhaoxinhao <zhaoxinhao2012@126.com>
2022-05-05 19:33:26 +0800

    add some test file

--- experiments/0_dataprocess/get_reference.sh
+++ experiments/0_dataprocess/get_reference.sh
@@ -1,4 +1,4 @@
 python experiments/0_dataprocess/code/get_reference.py \
 	--dataset CNewSum \
-	--file_path /home/zhaoxinhao/data2/cpm1/data/CNewSum/test.simple.label.jsonl \
-	--output_file_path /home/zhaoxinhao/data2/cpm1/reference_data/CNewSum/test.txt
+	--file_path /home/zhaoxinhao/data2/cpm1/data/CNewSum/dev.simple.label.jsonl \
+	--output_file_path /home/zhaoxinhao/data2/cpm1/reference_data/CNewSum/dev.txt
