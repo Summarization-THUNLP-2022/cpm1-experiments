1f2063e788881641e19210ffba405e65bee642c9
zhaoxinhao <zhaoxinhao2012@126.com>
2022-05-09 20:31:44 +0800

    modify pretokenize scripts

--- experiments/20220507_1/pretokenize_candidates.sh
+++ experiments/20220507_1/pretokenize_candidates.sh
@@ -1,8 +1,8 @@
 BASE_PATH=$(cd $(dirname "${BASH_SOURCE[0]}") >/dev/null && pwd)
 
-INPUT_FILE="/home/zhaoxinhao/data2/cpm1/train_data/CNewSum/train.simple.label.jsonl.900"
-CANDIDATES_FILE="/home/zhaoxinhao/data2/cpm1/experiments/20220505_1/result_for_brio/6-0.jsonl.sorted.txt.sorted"
-OUTPUT_FILE="/home/zhaoxinhao/data2/cpm1/train_data/CNewSum/brio.train.simple.label.jsonl.900"
+INPUT_FILE="${CPM_TRAIN_DATA_PATH}/CNewSum/train.simple.label.jsonl.900"
+CANDIDATES_FILE="${CPM_BASE_PATH}/experiments/20220505_1/result_for_brio/6-0.jsonl.sorted.txt.sorted"
+OUTPUT_FILE="${CPM_TRAIN_DATA_PATH}/CNewSum/brio.train.simple.label.jsonl.900"
 
 python ${BASE_PATH}/code_data/pretokenize.py \
 	${INPUT_FILE} \
